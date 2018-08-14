97b5f2db2c94de8cd923fdaaa121fae4e749b7f0
Silvia Piovesan <silvia.piovesan@gmail.com>
2018-08-14 20:57:38 +0200

    backstage text

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,7 @@ import { Carousel } from "react-bootstrap";
 import "./../css/home.css";
 import { translate } from "react-i18next";
 import PropTypes from "prop-types";
+import {Grid, Row, PageHeader, Col} from "react-bootstrap";
 import CountDown from "./CountDown";
 
 class About extends Component {
@@ -14,10 +15,29 @@ class About extends Component {
         this.props.setActiveNav("/about");
     }    
   
-    render() {              
+    render() {
+        const {props:{t}} = this;              
         return (
-            <div>       
-                Back stage section
+            <div>
+                <Grid >
+                    <Row><PageHeader className="App">Backstage</PageHeader> </Row>
+                    <Row>
+                        <Col xs={12}>       
+                            {t("silvia1")}
+                            <br/>{t("elia1")}
+                            <br/>{t("silvia2")}
+                            <br/>{t("elia2")}
+                            <br/>{t("challenge")}
+                            <br/>{t("suggestion")}
+                            <br/>{t("learnings")}
+                            <br/>{t("conclusion")} {" "} <i class="fa fa-smile-o"></i>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12}>
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
             
         );
--- src/i18n.js
+++ src/i18n.js
@@ -19,7 +19,8 @@ i18n
                     Home: "Home",
                     Logistics: "The Wedding",
                     Wall: "Chat with us",
-                    RSVP: "RSVP"
+                    RSVP: "RSVP",
+                    About: "Backstage",
                 },
                 home: {
                     "Welcome": "Welcome",
@@ -64,6 +65,16 @@ i18n
                 },
                 logistics:{
                   
+                },
+                about:{
+                    silvia1: "- Elia, what should we use to create our wedding page? Joy, Zankyou…?",
+                    elia1: "- I beg your pardon?!",
+                    silvia2: "- Oh dear, you don’t want to do it from scratch, do you?",
+                    elia2: "- Sure I do! A developer like me would never ever use a website built by someone else!",
+                    challenge: " It was with this conversation about pride, some weeks ago, that our adventure began: build our webpage from scratch. Yes, you got it, we didn’t use a single template: from the Favicon to the Chat, we practically coded it all.",
+                    suggestion: "No need of lying here, this website cost us a lot of hours and made us sweat (literally: we did this when there was the heat wave). For this reason, if you have some suggestions, we can’t guarantee we will have the strength to apply them, but you can still try to send them through the chat.",
+                    learnings: "Building a website also taught (or reminded) us a lot of things: that two is better than one, and it’s better to suffer together than alone; that appearances sometimes don’t matter, and Java has nothing to do with JavaScript; that some other times, they do matter, and suddenly you lose an hour trying to paint a button in blue; that if someone is important, it’s best to let them know (in CSS, using !important); that before pushing, you always have to pull; that security is important, and if you thought no one was controlling you, you were wrong, because Git Guardian is always there; that with the appropriate means, miracles can be done, because Elia with a RAM and a solid HD made Windows10 start in less than 10 seconds; that if you want it, you can do it, and even if you never coded, you can become a JS Ninja; that needs are relative, because you can survive only on beer and chips.",
+                    conclusion: "But the best thing this little project confirmed, is that we are still able to have a lot of fun with little, and together we are the best. Check out our pictures if you don't believe us",
                 }               
             },
             it: {                
@@ -79,7 +90,8 @@ i18n
                     Home: "Home",
                     Logistics: "Il Matrimonio",
                     Wall: "Chatta con noi",
-                    RSVP: "RSVP"
+                    RSVP: "RSVP",
+                    About: "Backstage",
                 },
                 home: {
                     "Welcome": "Welcome",
@@ -123,6 +135,16 @@ i18n
                     confirmYourName: "Controlla che il tuo nome di Google ci permetta di capire chi sei :-)"
                 },
                 logistics:{
+                },
+                about:{
+                    silvia1: "- Elia, dove lo facciamo il sito per il nostro matrimonio? Joy, Zankyou…?",
+                    elia1: "- Scusa?",
+                    silvia2: "- Sì, beh, vorrai mica farlo da zero?!",
+                    elia2: "- Ma certo! Ti pare che io, informatico, possa usare un sito fatto da altri?",
+                    challenge: "Con questa manifestazione di orgoglio è iniziata, qualche settimana fa, la nostra impresa: fare il nostro sito da zero. Sì, avete letto bene, non è stato usato neanche un template: dalla Favicon alla Chat, è tutta farina del nostro sacco.",
+                    suggestion: "Non c’è bisogno di mentire, il sito ci è costato tante ore e tanto sudore (letteralmente: abbiamo beccato proprio i giorni del caldo anomalo). Per questo, se avete dei suggerimenti, non possiamo garantire che avremo le energie per applicarli, ma potete comunque provare a mandarceli attraverso la chat.",
+                    learnings: "Fare un sito ci ha anche insegnato (o ricordato) tante cose: che, come diceva la pubblicità del Maxibon, 2 is megl che 1, e meglio soffrire in due che da soli; che le apparenze a volte non contano, e Java non c’entra nulla con JavaScript; che altre volte invece sì che contano, ed ecco che puoi perdere un’ora cercando di colorare un bottone di blu; che se qualcuno è importante, è meglio farglielo sapere (in CSS, utilizzando !important); che prima di fare push, devi sempre fare pull; che la sicurezza è importante, e se pensavi che nessuno ti stesse controllando, ti sbagliavi, perché Git Guardian sta sempre all’occhio; che con gli strumenti adatti si possono fare magie, perché Elia con una RAM e un hard disk solido è riuscito a far avviare Windows 10 in meno di 10 secondi; che volere è potere, e anche se non hai mai programmato, puoi diventare una JS Ninja; che le necessità sono relative, dato che si può sopravvivere con birra e patatine in sacchetto.",
+                    conclusion: "Ma la cosa più bella che questo progetto ci ha confermato è che siamo ancora capaci di  divertirci con poco, e che assieme siamo inarrestabili. Dai un'occhiata alle nostre foto se non ci credi",
                 }              
             },
             es: {                
@@ -138,7 +160,8 @@ i18n
                     Home: "Home",
                     Logistics: "La boda",
                     Wall: "Chat con los novios",
-                    RSVP: "RSVP"
+                    RSVP: "RSVP",
+                    About: "Backstage",
                 },
                 home: {
                     "Welcome": "Welcome",
@@ -181,6 +204,16 @@ i18n
                     confirmYourName: "Comprueba si el nombre que tienes en Google lo podemos entender :-)"
                 },
                 logistics:{
+                },
+                about:{
+                    silvia1: "- Elia, dónde hacemos la web para nuestra boda? Joy, ZankYou…?",
+                    elia1: "- El qué???",
+                    silvia2: "- Sí, bueno, no querrás hacerla de cero?",
+                    elia2: "- Obvio! Tu te crees que yo, informático, tenga que usar una web hecha por otros?",
+                    challenge: "Con esta manifestación de orgullo empezó, hace algunas semanas, nuestro reto: hacer nuestra web desde cero. Sí, habéis leído bien, no hemos usado ningún triste template: desde la Favicon al Chat, todo lo hemos hecho nosotros.",
+                    suggestion: "No hace falta mentir, eso nos costó tantas horas y tanto sudor (textualmente: hemos pillado justo los días de la ola de calor, imaginaros que queríamos ir a la oficina para tener aire acondicionado). Por lo tanto, si tenéis alguna sugerencia, no podemos garantizar que tengamos las energías para ponerla en práctica, pero igualmente podéis intentar enviaros vuestros comentarios a través del chat.",
+                    learnings: "Hacer una web también nos ha enseñado (o recordado) muchas cosas: que dos es mejor que uno, y es mejor sufrir en dos que solos; que el aspecto a veces no es importante, y que Java no tiene nada que ver con JavaScript; que otras veces sí que es importante, y más cuando pierdes una hora intentando colorear un botón de azul; que cuando alguien es importante, es mejor hacérselo saber (en CSS, usando !important); que antes de hacer push, siempre tienes que hacer pull; que la seguridad es importante, y si creías que nadie estuviera controlándote, te equivocabas, porque Git Guardian siempre está ahí; que con los instrumentos adecuados se pueden hacer milagros, porque Elia con una RAM y un disco duro sólido ha conseguido arrancar Windows 10 en menos de 10 segundos; que querer es poder, y aunque no hayas programado en la vida, puedes transformarte en una JS Ninja; que las necesidades son relativas, porque se puede sobrevivir con cerveza y patatas chips.",
+                    conclusion: "Pero la cosa mejor que este proyecto nos confirmó, es que todavía somos capaces de pasarlo bien con poca cosa, y que juntos somos lo mejor. Echa un vistazo a nuestras fotos si no te lo crees",
                 }                
             }                  
         },
@@ -188,7 +221,7 @@ i18n
         debug: true,
 
         // have a common namespace used around the full app
-        ns: ["translations", "navigation", "home"],
+        ns: ["translations", "navigation", "home", "about"],
         defaultNS: "navigation",
 
         keySeparator: false, // we use content as keys
